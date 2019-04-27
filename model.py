20e60072fadba1c9fd6656f19b1489f7f47912e4
Katrina Evtimova <kve216@nyu.edu>
2019-04-27 14:44:11 -0400

    sample Model class

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def load_model(self, pretrained_model_path, cuda=True):
+        # Load pretrained model
+        pretrained_model = torch.load(f=pretrained_model_path, map_location="cuda" if cuda else "cpu")
+        self.load_state_dict(pretrained_model, strict=True)
+
+        # Debug loading
+        print('Parameters found in pretrained model:')
+        pretrained_layers = pretrained_model.keys()
+        for l in pretrained_layers:
+            print('\t' + l)
+        print('')
+
+        for name, module in self.state_dict().items():
+            if name in pretrained_layers:
+                assert torch.equal(pretrained_model[name].cpu(), module.cpu())
+                print('{} have been loaded correctly in current model.'.format(name))
+
+    def forward(self):
+        raise NotImplementedError
